3f5ecae018051a7fa4e75846cf660c6337357d55
zsjnb <33953490+zsjnb@users.noreply.github.com>
2019-02-01 14:05:21 +0800

    Update MonitorApplication.java

--- monitor/src/main/java/cn/zhangxd/monitor/MonitorApplication.java
+++ monitor/src/main/java/cn/zhangxd/monitor/MonitorApplication.java
@@ -39,9 +39,7 @@ public class MonitorApplication {
             http.csrf().disable();
 
             // Requests for the login page and the static assets are allowed
-            http.authorizeRequests()
-                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
-                    .permitAll();
+            http.authorizeRequests().antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**").permitAll();
             // ... and any other request needs to be authorized
             http.authorizeRequests().antMatchers("/**").authenticated();
 
