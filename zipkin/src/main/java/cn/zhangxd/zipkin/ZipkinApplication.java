6a6f8b2d5bfcb2fa2ca3f65c3445771ac88b4916
zsjnb <33953490+zsjnb@users.noreply.github.com>
2019-02-01 14:15:54 +0800

    Update ZipkinApplication.java
    new EnableZipkinServer

--- zipkin/src/main/java/cn/zhangxd/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/cn/zhangxd/zipkin/ZipkinApplication.java
@@ -2,13 +2,16 @@ package cn.zhangxd.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+import zipkin.server.internal.EnableZipkinServer;
 
 /**
- * @author zhangxd
+ * SpringCloud Edgware之前的版本使用Zipkin 1.x，要想MQ方式收集数据需整合spring-cloud-sleuth-stream
+ * 而在Edgware及更高版本中，使用Zipkin 2.x本身已支持基于MQ的数据收集方式，而spring-cloud-sleuth-stream将被废弃
+ * http://www.itmuch.com/spring-cloud/edgware-new-sleuth-zipkin-mq/
  */
 @SpringBootApplication
-@EnableZipkinStreamServer
+@EnableZipkinServer
 public class ZipkinApplication {
 
     public static void main(String[] args) {
